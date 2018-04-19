416148d620e124f334a840fa4509fb184d849cc2
Abel Georges <abelgr8@gmail.comgit config --global user.name Abel>
2018-04-19 17:52:26 -0400

    puahing app

---
+++ app/data/friends.js
@@ -0,0 +1,191 @@
+var friends = [
+    {
+        name: "Buster Baxter",  
+        photo: "https://vignette.wikia.nocookie.net/arthur/images/6/65/Buster_Baxter.png/revision/latest?cb=20160720053811",
+        scores : [
+            "5",
+            "4",
+            "1",
+            "4",
+            "5",
+            "2",
+            "3",
+            "5",
+            "3",
+            "1",
+        ]
+    },
+
+    {
+        name: "Arthur Read",  
+        photo: "https://vignette.wikia.nocookie.net/arthur/images/0/0b/S7E3a_Arthur_Read.png/revision/latest?cb=20160720053956",
+        scores : [
+            "5",
+            "5",
+            "3",
+            "4",
+            "4",
+            "1",
+            "2",
+            "3",
+            "5",
+            "1",
+        ]
+    },
+
+    {
+        name: "D.W. Read",  
+        photo: "https://vignette.wikia.nocookie.net/arthur/images/0/09/DW_S8E06.png/revision/latest/scale-to-width-down/250?cb=20171007112002",
+        scores : [
+            "5",
+            "1",
+            "1",
+            "2",
+            "2",
+            "4",
+            "5",
+            "1",
+            "1",
+            "5",
+        ]
+    },
+
+    {
+        name: "Francine Frensky",  
+        photo: "https://vignette.wikia.nocookie.net/arthur/images/6/64/Francine_706b.png/revision/latest/scale-to-width-down/250?cb=20160903120046",
+        scores : [
+            "5",
+            "5",
+            "5",
+            "3",
+            "1",
+            "3",
+            "5",
+            "2",
+            "2",
+            "3",
+        ]
+    },
+
+    {
+        name: "Alan 'The Brain' Powers",  
+        photo: "https://vignette.wikia.nocookie.net/arthur/images/f/f3/Brain_Arthur_Weighs_In.png/revision/latest?cb=20160720055849",
+        scores : [
+            "5",
+            "5",
+            "5",
+            "1",
+            "5",
+            "4",
+            "5",
+            "5",
+            "3",
+            "2",
+        ]
+    },
+
+    {
+        name: "Sue Ellen Armstrong",  
+        photo: "https://vignette.wikia.nocookie.net/arthur/images/b/b0/Sue_Ellen.png/revision/latest/scale-to-width-down/250?cb=20150102091156",
+        scores : [
+            "5",
+            "3",
+            "2",
+            "1",
+            "3",
+            "1",
+            "3",
+            "1",
+            "3",
+            "2",
+        ]
+    },
+
+    {
+        name: "Muffy Crosswire",  
+        photo: "https://vignette.wikia.nocookie.net/arthur/images/2/20/April_9th_67.JPG/revision/latest/scale-to-width-down/250?cb=20150805080231",
+        scores : [
+            "5",
+            "1",
+            "5",
+            "2",
+            "2",
+            "4",
+            "5",
+            "1",
+            "5",
+            "5",
+        ]
+    },
+
+    {
+        name: "Binky Barnes",  
+        photo: "https://vignette.wikia.nocookie.net/arthur/images/9/9f/Binky_s9_better.png/revision/latest/scale-to-width-down/250?cb=20160309042205",
+        scores : [
+            "1",
+            "1",
+            "1",
+            "3",
+            "1",
+            "5",
+            "5",
+            "3",
+            "1",
+            "1",
+        ]
+    },
+
+    {
+        name: "Prunella Deegan",  
+        photo: "https://vignette.wikia.nocookie.net/arthur/images/7/7f/Prunella.png/revision/latest/scale-to-width-down/250?cb=20160604095245",
+        scores : [
+            "5", //1
+            "5", //2
+            "5", //3
+            "1", //4
+            "5", //5
+            "1", //6
+            "5", //7
+            "1", //8
+            "5", //9
+            "5", //10
+        ]
+    },
+
+    {
+        name: "Fern Walters",  
+        photo: "https://vignette.wikia.nocookie.net/arthur/images/a/a3/Fern_Walters.png/revision/latest/scale-to-width-down/250?cb=20150714011141",
+        scores : [
+            "5", //1
+            "5", //2
+            "1", //3
+            "4", //4
+            "5", //5
+            "1", //6
+            "5", //7
+            "2", //8
+            "5", //9
+            "1", //10
+        ]
+    },
+
+    {
+        name: "George Lundgren",  
+        photo: "https://vignette.wikia.nocookie.net/arthur/images/7/77/George_s6.png/revision/latest/scale-to-width-down/250?cb=20150124233536",
+        scores : [
+            "1", //1
+            "1", //2
+            "1", //3
+            "1", //4
+            "5", //5
+            "1", //6
+            "3", //7
+            "1", //8
+            "5", //9
+            "1", //10
+        ]
+    },
+
+]
+
+module.exports = friends;
